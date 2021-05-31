f11c1b0ba50d8650b743a8468c988e1d3337e105
ANIKSHA S <84078705+Aniksha02@users.noreply.github.com>
2021-06-01 01:27:10 +0530

    Update distance_formula.c

--- distance_formula.c
+++ distance_formula.c
@@ -1,16 +1,1 @@
-#include<stdio.h>
-#include<stdlib.h>
 
-int x1,x2,y1,y2;
-int d;
-
-void main()
-{
-    printf("enter x an y values ");
-    scanf("%d%d%d%d",&x1,&x2,&y1,&y2);
-
-    d = sqrt(pow((x2-x1),2) +pow((x2 -x1),2));
-
-    printf("the distance btw the lines are %f",d);
-    getch();
-}
