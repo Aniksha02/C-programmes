208f93895b16f433302e40a872f34f3eb6fa3058
ANIKSHA S <84078705+Aniksha02@users.noreply.github.com>
2021-06-01 00:45:08 +0530

    Add files via upload

---
+++ distance_formula.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int x1,x2,y1,y2;
+float d;
+
+void main()
+{
+    printf("enter x an y values ");
+    scanf("%d%d%d%d",&x1,&x2,&y1,&y2);
+
+    d = sqrt(pow((x2-x1),2) +pow((x2 -x1),2));
+
+    printf("the distance btw the lines are %f",d);
+    getch();
+}
