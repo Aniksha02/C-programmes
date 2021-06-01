d51b227c1c4ac52e61d08a6c8f4527d6765c4a6b
ANIKSHA S <84078705+Aniksha02@users.noreply.github.com>
2021-06-02 00:13:39 +0530

    Update circle.c

--- circle.c
+++ circle.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
 void main()
 {
-float radius,diameter,circumference,area,pi=3.14;
-printf("Enter the radius of the circle: \n");
-scanf("%f",&radius);
-if(radius>3)
+    
+      int radius,diameter,circumference,area,pi=3.14;
+    
+    printf("Enter the radius of the circle: \n");
+    scanf("%d",&radius);
+
+    if(radius>3)
 {
-diameter=2*radius;
-circumference=2*pi*radius;
-area=pi*radius*radius;
-printf("Diameter= %f \n Circumference=%f \n Area=%f",diameter,circumference,area);
+    diameter=2*radius;
+   circumference=2*pi*radius;
+   area=pi*radius*radius;
+
+        printf("Diameter= %d \n Circumference=%d \n Area=%d",diameter,circumference,area);
 }
-else{
-    printf("Error,radius too small");
+else{printf("Error,radius too small");
 }
 }
