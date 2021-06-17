47423443cdceb9a55dd043a4077d0f74014a8775
ANIKSHA S <84078705+Aniksha02@users.noreply.github.com>
2021-06-17 14:28:35 +0530

    Update Marks.c

--- Marks.c
+++ Marks.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
 void main()
 {
-int marks[4][3],highest[4],i,j;
-for(i=0;i<4;i++)
-{printf("Enter marks in subject %d : \n ",(i+1));
+     int marks[4][3],highest[4],i,j;
+  
+for(i=0;i<4;i++) 
+  
+{     printf("Enter marks in subject %d : \n ",(i+1));
 for(j=0;j<3;j++)
-{scanf("%d",&marks[i][j]);}
+      {scanf("%d",&marks[i][j]);}
 }
 for(i=0;i<4;i++)
-{highest[i]=marks[i][0];
+
+     {highest[i]=marks[i][0];
 for(j=1;j<3;j++)
 {if(marks[i][j]>highest[i])
-{highest[i]=marks[i][j];}
+    {highest[i]=marks[i][j];}
 }
 }
 for(i=0;i<4;i++)
-{printf("Highest marks in subject %d = %d \n",i,highest[i]);}
+{printf("Highest marks in subject %d = %d \n",i+1,highest[i]);}
 }
